87c4fda10d253897065decab2dad9dff6ad40660
Jose Angel Hernandez <joseanher81@gmail.com>
2023-09-25 09:12:17 +0200

    functions for formatting board data for firestore

--- src/helpers/formatActivities.js
+++ src/helpers/formatActivities.js
@@ -1,17 +1,39 @@
 /* This function receives a trip and format its day activities
  in order to be consumed by react-beautiful-dnd logic */
 
-export const formatActivities = (trip) => {
+export const formatActivitiesForBoard = (trip) => {
 
-    const daysArr = trip.days;
     const columns = {}
 
-    for(let i = 0; i< daysArr.length; i++) {
-        columns[daysArr[i].id] = {title: daysArr[i].id,items: daysArr[i].activities.map(activity => ({id: activity, Task: activity}))};
-    }
+    const days = Object.keys(trip.days);
+    
+    days.forEach(day => {
+        console.log('TRIPDAYS', day, trip.days[day])
+        columns[day] = {title: day, items: trip.days[day].map( activity => ({id: `${day} - ${activity.pos}`, Task: activity.activity}))}
+    });
 
-    console.log('COLUMNS',JSON.stringify(columns) );
+    console.log(' UNSORTED COLUMNS',JSON.stringify(columns));
+    console.log(' SORTED COLUMNS',JSON.stringify(sortDays(columns)));
 
-    return columns;
+    return sortDays(columns);
 }
 
+export const formatActivitiesForFirebase = (columns) => {
+    const keys = Object.keys(columns);
+  
+    const result = {};
+    keys.forEach( day => result[day] = columns[day].items.map( (act, index) => ({pos: index, activity: act.Task })));
+
+    const update = {'days': result};
+
+    return update;
+}
+
+const sortDays = (data) => {
+    return Object.keys(data)
+      .sort() // Ordena las claves alfabéticamente
+      .reduce((acc, key) => {
+        acc[key] = data[key];
+        return acc;
+      }, {})
+  };
