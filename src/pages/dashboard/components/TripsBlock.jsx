db26fe9a3f9c7f97699343fcc7e0c5ed137266c4
Jose Angel Hernandez <joseanher81@gmail.com>
2023-10-08 15:51:30 +0200

    added no trips text in dashboard

--- src/pages/dashboard/components/TripsBlock.jsx
+++ src/pages/dashboard/components/TripsBlock.jsx
@@ -24,14 +24,16 @@ export default function TripsBlock({user, title, future, trips}) {
             {title}
           </Typography>
           <Typography variant="h5" align="center" color="text.secondary" paragraph>
-          {future && 
+          {(future && trips) &&
             (<>¡Hola, {user.displayName}! Aquí están todos tus viajes en un solo lugar, listos para ser explorados. Disfruta de la libertad de planificar, soñar y descubrir nuevos destinos. 
             Tu próxima aventura te espera. ¡Empieza a crear recuerdos inolvidables ahora!</>)
           }
-          {!future && 
+          {(!future && trips) &&
             (<>{user.displayName}, estos son los viajes que ha yas disfrutado. ¡Recuerda todos los grandes momentos!</>)
           }
-
+          {!trips && 
+            (<>Aún no tienes ningún viaje a la vista. ¿A qué esperas para crear tu primera aventura?</>)
+          }
           </Typography>
 
         </Container>
