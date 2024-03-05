b0ca97764e7022a48d3ef184fd4fac4c1b92ca6e
Jose Angel Hernandez <joseanher81@gmail.com>
2024-03-05 19:39:38 +0100

    Improved spacing of companions component in overview

--- src/pages/overview/Stats.tsx
+++ src/pages/overview/Stats.tsx
@@ -52,19 +52,19 @@ const Stats = (props: StatsProps) => {
                         {!companions && <Typography variant="h1"  mt={'30px'} color="secondary" fontWeight="bold">You traveled on your own!</Typography>}
 
                         {companions && (
-                            <Grid container spacing={2} sx={{marginTop: '10px', justifyContent: 'center'}} >
-                                {companions.map((companion, index) => 
-                                    user.uid !== companion.id && 
-                                    (
+                            <Grid container spacing={2} sx={{marginTop: '0', justifyContent: 'center' }}>
+                                {companions.map((companion, index) => (
+                                    user.uid !== companion.id && (
                                         <Grid item xs={2} key={index}>
                                             <Grid container direction="column" alignItems="center">
-                                                <Avatar alt={companion.displayName} src={companion.photoURL} sx={{ width: 100, height: 100 }} />
+                                                <Avatar alt={companion.displayName} src={companion.photoURL} sx={{ width: 80, height: 80 }} />
                                                 <Typography variant="subtitle1" align="center">
                                                     {companion.displayName}
                                                 </Typography>
                                             </Grid>
                                         </Grid>
-                                    ))}
+                                    )
+                                ))}
                             </Grid>
                         )}
 
